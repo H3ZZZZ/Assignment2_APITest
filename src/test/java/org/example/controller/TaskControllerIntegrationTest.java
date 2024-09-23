33bfe58af22f23741320ab2fc9e7d83370900ca8
Emilio Lagunas <Cph-ec100@cphbusiness.dk>
2024-09-23 12:11:19 +0200

    removed some snake_case

--- src/test/java/org/example/controller/TaskControllerIntegrationTest.java
+++ src/test/java/org/example/controller/TaskControllerIntegrationTest.java
@@ -40,7 +40,7 @@ public class TaskControllerIntegrationTest {
     }
 
     @Test
-    public void testGetTasks_ReturnsListOfTasks() throws Exception {
+    public void testGetTasksReturnsListOfTasks() throws Exception {
         // Arrange: Create and save tasks in the repository
         Task task1 = new Task(null, "Title 1", "Description 1", LocalDate.now(), "Category 1");
         Task task2 = new Task(null, "Title 2", "Description 2", LocalDate.now(), "Category 2");
@@ -55,7 +55,7 @@ public class TaskControllerIntegrationTest {
     }
 
     @Test
-    public void testAddTask_ValidTask_ReturnsCreatedTask() throws Exception {
+    public void testAddTaskValidTaskReturnsCreatedTask() throws Exception {
         // Arrange: Create a task
         Task task = new Task(null, "Title", "Description", LocalDate.now(), "Category");
 
@@ -68,7 +68,7 @@ public class TaskControllerIntegrationTest {
             .andExpect(jsonPath("$.description", is("Description")));  // Check the task's description
     }
     @Test
-    public void testUpdateTask_ValidTask_ReturnsUpdatedTask() throws Exception {
+    public void testUpdateTaskValidTaskReturnsUpdatedTask() throws Exception {
         // Arrange: Create and save a task in the repository
         Task task = new Task(null, "Title", "Description", LocalDate.now(), "Category");
         taskRepository.save(task);
@@ -86,7 +86,7 @@ public class TaskControllerIntegrationTest {
             .andExpect(jsonPath("$.description", is("Updated Description")));  // Check the updated description
     }
     @Test
-    public void testDeleteTask_ValidTask_ReturnsNoContent() throws Exception {
+    public void testDeleteTaskValidTaskReturnsNoContent() throws Exception {
         // Arrange: Create and save a task in the repository
         Task task = new Task(null, "Title", "Description", LocalDate.now(), "Category");
         taskRepository.save(task);
@@ -102,7 +102,7 @@ public class TaskControllerIntegrationTest {
     }
 
     @Test
-    public void testMarkAsCompleted_ValidTask_ReturnsCompletedTask() throws Exception {
+    public void testMarkAsCompletedValidTaskReturnsCompletedTask() throws Exception {
         // Arrange: Create and save a task in the repository
         Task task = new Task(null, "Title", "Description", LocalDate.now(), "Category");
         taskRepository.save(task);
